60e6fa9559396ecd456bed22104feb944ef62c3b
Gilles <43683714+corp-0@users.noreply.github.com>
2025-04-19 18:03:32 -0400

    refactor: optimises balance calculation by skipping redundancy

--- src/transactions/models.py
+++ src/transactions/models.py
@@ -1,25 +1,7 @@
 from decimal import Decimal
-
 from django.utils import timezone
-
 from django.db import models
 
-
-def recalculate_balances():
-    """Recalculate the balance after each movement. This way the balance is updated even if you add a movement that happened in the past."""
-
-    movements = Movement.objects.all().order_by("created_at")
-    balance = Decimal('0.00')
-
-    for movement in movements:
-        if movement.type == 'income':
-            balance += movement.amount_usd
-        else:
-            balance -= movement.amount_usd
-
-        if movement.balance_after != balance:
-            movement.balance_after = balance
-            movement.save(update_fields=['balance_after'])
 
 MOVEMENT_TYPES = [
     ("income", "Income"),
@@ -35,19 +17,16 @@ class Movement(models.Model):
     amount_usd = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(default=timezone.now)
     balance_after = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    link = models.URLField(blank=True, null=True)
 
     class Meta:
-        ordering = ['created_at']
+        ordering = ['-created_at']
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) -> None:
         is_new = self._state.adding
-
         if is_new:
-            # Temporarily set balance_after to 0 to avoid NULL error
-            self.balance_after = Decimal("0.00")
-
-        super().save(*args, **kwargs)  # Save once so the row exists (with a temporary value)
-        recalculate_balances()
+            self.balance_after = Decimal("0.00")  # Placeholder
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.created_at.date()} - {self.type.capitalize()}: ${self.amount_usd} - {self.description}"
